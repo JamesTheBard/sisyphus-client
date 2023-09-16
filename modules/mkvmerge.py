584c71341f3cb79cc853e29a9890131084334856
jweatherly <jamesthebard@gmail.com>
2023-09-16 11:51:52 -0400

    Cleaning up naming

--- modules/mkvmerge.py
+++ modules/mkvmerge.py
@@ -1,38 +1,38 @@
 from box import Box
 from jsonschema import exceptions as JsonExceptions
 from loguru import logger
-from matroska import Matroska as M
+from mkvmerge import Mkvmerge as M
 
 from app.exceptions import RunError, ValidationError
 from modules.base import BaseModule
 
 
-class Matroska(BaseModule):
+class Mkvmerge(BaseModule):
     def __init__(self, task):
         super().__init__(task)
         logger.info("Module loaded successfully.")
         logger.debug(f"Data: {self.task}")
         self.status = Box({
             "status": "in_progress",
-            "task": "matroska"
+            "task": "mkvmerge"
         })
         self.heartbeat.set_data(self.status)
-        self.matroska = M()
+        self.mkvmerge = M()
 
     def validate(self):
         try:
-            self.matroska.load_from_object(self.task)
+            self.mkvmerge.load_from_object(self.task)
         except JsonExceptions.ValidationError as e:
             raise ValidationError(e.message)
 
         logger.info("Task data validated successfully.")
 
     def run(self):
-        command = self.matroska.generate_command(as_string=True)
+        command = self.mkvmerge.generate_command(as_string=True)
         logger.debug("Command to run: {command}")
         logger.info("Running mkvmerge muxing task")
 
-        return_code = self.matroska.mux(delete_temp=True)
+        return_code = self.mkvmerge.mux(delete_temp=True)
         if return_code != 0:
             raise RunError(
                 f"The `mkvmerge` command returned exit code {return_code}, command: {command}")
