755eeacad1967a407a52d1254e6ccb5e8a1f23df
JamesTheBard <jamesthebard@gmail.com>
2023-10-07 23:05:56 -0400

    Moved config information into config file.

--- app/config.py
+++ app/config.py
@@ -14,11 +14,16 @@ with open("pyproject.toml", 'rb') as f:
 class Config:
     API_URL = os.environ.get("API_URL", "http://localhost:5000")
     API_TIMEZONE = ZoneInfo(os.environ.get("API_TIMEZONE", "UTC"))
-    VERSION = pyproject.tool.poetry.version
+    VERSION = "1.5.0a1"
     HOSTNAME = os.environ.get("HOSTNAME_OVERRIDE", platform.node())
     HOST_UUID = os.environ.get("HOST_UUID", str(uuid.uuid4()))
     HEARTBEAT_INTERVAL = int(os.environ.get("HEARTBEAT_INTERVAL", "5"))
     QUEUE_POLL_INTERVAL = int(os.environ.get("QUEUE_POLL_INTERVAL", "10"))
     NETWORK_RETRY_INTERVAL = int(
         os.environ.get("NETWORK_RETRY_INTERVAL", "20"))
-    MODULES = pyproject.tool.client.modules.enabled
+    MODULES = {
+        "ffmpeg": "modules.ffmpeg.Ffmpeg",
+        "mkvmerge": "modules.mkvmerge.Mkvmerge",
+        "mkvextract": "modules.mkvextract.Mkvextract",
+        "cleanup": "modules.cleanup.Cleanup",
+    }
